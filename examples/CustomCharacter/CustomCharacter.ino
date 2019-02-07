513197f32cc4358797ed85105104138f9fe8e9f6
Electronza <blog@electronza.com>
2019-02-07 11:49:36 +0200

    Update CustomCharacter.ino
    Modified example for MCP23S17 port expander

--- examples/CustomCharacter/CustomCharacter.ino
+++ examples/CustomCharacter/CustomCharacter.ino
@@ -1,49 +1,72 @@
 /*
-  STEMMA_LiquidCrystal Library - Custom Characters
+  MCP23S17 LCD Library - Custom Characters
 
- Demonstrates how to add custom characters on an LCD  display.
- The STEMMA_LiquidCrystal library works with all LCD displays that are
- compatible with the  Hitachi HD44780 driver. There are many of
- them out there, and you can usually tell them by the 16-pin interface.
+ Demonstrates the use a 16x2 LCD display.  The MCP23S17
+ library works with all LCD displays that are compatible with the
+ Hitachi HD44780 driver. There are many of them out there, and you
+ can usually tell them by the 16-pin interface.
 
  This sketch prints "I <heart> Arduino!" and a little dancing man
  to the LCD.
 
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
+ The LCD can be connected either to GPIO port A or B of MCP23S17
+ NOTA: LCD works in 4-bit mode only and it requires 
+ the following connections:
+
+ * LCD D7 pin  to GPx7
+ * LCD D6 pin  to GPx6
+ * LCD D5 pin  to GPx5
+ * LCD D4 pin  to GPx4
+ * LCD EN pin  to GPx3
+ * LCD RS pin  to GPx2
  * LCD R/W pin to ground
- * 10K potentiometer:
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
  * ends to +5V and ground
  * wiper to LCD VO pin (pin 3)
- * 10K poterntiometer on pin A0
+ 
+ Where GPx is either GPA or GPB of MCP23S17
 
- created 21 Mar 2011
+ Library originally added 18 Apr 2008
+ by David A. Mellis
+ library modified 5 Jul 2009
+ by Limor Fried (http://www.ladyada.net)
+ example added 9 Jul 2009
  by Tom Igoe
- modified 11 Nov 2013
- by Scott Fitzgerald
+ modified 22 Nov 2010
+ by Tom Igoe
  modified 7 Nov 2016
  by Arturo Guadalupi
-
- Based on Adafruit's example at
- https://github.com/adafruit/SPI_VFD/blob/master/examples/createChar/createChar.pde
+ modified 7 Feb 2019
+ by Teodor Costachioiu (https://electronza.com)
 
  This example code is in the public domain.
- http://www.arduino.cc/en/Tutorial/STEMMA_LiquidCrystalCustomCharacter
 
- Also useful:
- http://icontexto.com/charactercreator/
+ http://electronza.com/mcp23s17-lcd-library
 
 */
 
 // include the library code:
-#include <STEMMA_LiquidCrystal.h>
+#include <MCP23S17_LCD.h>
 
-STEMMA_LiquidCrystal lcd;
+/* The LCD library is initialized as follows:
+ * MCP23S17_LCD(uint8_t rst, uint8_t cs, uint8_t PORT);
+ * rst  - RST pin for MCP23S17
+ * cs   - CS pin for MCP23S17 
+ * PORT - GPIO port of MCP23S17, where the LCD is connected
+ * PORT can be either PORT A or PORTB 
+ * 
+ * Example for MikroElektronika Expand click, connected via
+ * Arduino Uno Click shield, in mikroBUS socket #1
+ * and configured via J3 for 5V operation:
+ * https://www.mikroe.com/expand-click
+ * https://www.mikroe.com/arduino-uno-click-shield
+ * 
+ * MCP23S17_LCD lcd(A3, 10, PORTB);
+*/
+
+MCP23S17_LCD lcd(A3, 10, PORTB);
 
 // make some custom characters:
 byte heart[8] = {
